78601f3b5a1c5b3b606b52579ce4561aeb9ebf65
moeb8001 <moh.ebroh@gmail.com>
2023-12-14 12:02:56 +0100

    recodings

--- pwm_setter.py
+++ pwm_setter.py
@@ -10,7 +10,7 @@ uri = 'radio://0/80/2M/E7E7E7E7E7'
 
 #use "sudo -E python3 pwm_setter.py" to run this script
 
-motorvar = 55000
+motorvar = 40000
 latest_value = motorvar
 running = True
 motor_on = False
