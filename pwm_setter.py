32034c48028d0f4cc9113ec47add48c16da06197
= <h.licai@student.tudelft.nl>
2024-05-06 19:07:58 +0200

    test fix

--- pwm_setter.py
+++ pwm_setter.py
@@ -48,9 +48,9 @@ keyboard.on_press_key("o", toggle_motorvar)
 keyboard.on_press_key("n", quit_loop)
 
 def set_motor_power(scf, motor_index, pwm_value):
+
     # Enable motor power
     scf.cf.param.set_value("motorPowerSet.enable", 1)
-    
     # Set motor power
     motor_name = f"motorPowerSet.m{motor_index}"
     scf.cf.param.set_value(motor_name, pwm_value)
@@ -58,7 +58,6 @@ def set_motor_power(scf, motor_index, pwm_value):
 def simple_log(scf, logconf):
 
     with SyncLogger(scf, logconf) as logger:
-
         for log_entry in logger:
             data = log_entry[1]
             logconf_name = log_entry[2]
@@ -70,14 +69,11 @@ if __name__ == "__main__":
     # Initialize low level drivers
     cflib.crtp.init_drivers()
 
-
     battery_log = LogConfig(name='pm', period_in_ms=1000)
     battery_log.add_variable('pm.vbat', 'float')
 
     with SyncCrazyflie(uri, cf=Crazyflie(rw_cache="./cache")) as scf:
-
         print("CONNECTED! Press up/down to increase/decrease motor power, o to toggle motor on/off, n to quit")
-        
         while running:
             time.sleep(0.5)
             simple_log(scf, battery_log)
